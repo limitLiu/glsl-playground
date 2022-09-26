c33eecbc0f1ddbcb0c26fd7d2ac692909d65e901
limit <limitliu@qq.com>
2022-09-27 02:37:56 +0800

    chore(Cargo.toml): update 3rd party pkgs version

--- src/utils/common.rs
+++ src/utils/common.rs
@@ -4,12 +4,13 @@ use std::os::raw::c_void;
 use std::path::Path;
 
 use image::DynamicImage::{ImageLuma8, ImageLumaA8, ImageRgb8, ImageRgba8};
-use image::GenericImageView;
 
-pub unsafe fn load_texture(path: &str) -> u32 {
+pub fn load_texture(path: &str) -> u32 {
     let mut texture_id = 0;
-    gl::GenTextures(1, &mut texture_id);
-    let img = image::open(&Path::new(path)).expect("Failed to load texture");
+    unsafe {
+        gl::GenTextures(1, &mut texture_id);
+    };
+    let img = image::open(Path::new(path)).expect("Failed to load texture");
 
     let format = match img {
         ImageLuma8(_) => gl::RED,
@@ -19,29 +20,27 @@ pub unsafe fn load_texture(path: &str) -> u32 {
         _ => 0,
     };
     //    let img = img.flipv();
-    let data = img.to_bytes();
-    gl::BindTexture(gl::TEXTURE_2D, texture_id);
-    gl::TexImage2D(
-        gl::TEXTURE_2D,
-        0,
-        format as i32,
-        img.width() as i32,
-        img.height() as i32,
-        0,
-        format,
-        gl::UNSIGNED_BYTE,
-        &data[0] as *const u8 as *const c_void,
-    );
-    gl::GenerateMipmap(gl::TEXTURE_2D);
+    let data = img.as_bytes().to_vec();
+    unsafe {
+        gl::BindTexture(gl::TEXTURE_2D, texture_id);
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            format as i32,
+            img.width() as i32,
+            img.height() as i32,
+            0,
+            format,
+            gl::UNSIGNED_BYTE,
+            &data[0] as *const u8 as *const c_void,
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
 
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
-    gl::TexParameteri(
-        gl::TEXTURE_2D,
-        gl::TEXTURE_MIN_FILTER,
-        gl::LINEAR_MIPMAP_LINEAR as i32,
-    );
-    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+    };
 
     texture_id
 }
